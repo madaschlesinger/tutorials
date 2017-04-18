012f564a1b90444f44a99f8a9b7c4a9b44662934
madaschlesinger <mada.schlesinger@gmail.com>
2017-04-17 23:48:49 -0400

    shared lib support, deps, rules for install

--- make/makefile_rules.mk
+++ make/makefile_rules.mk
@@ -1,16 +1,9 @@
 
 
 # for loop....
-#BINS=$(BIN1) $(BIN2)
-
 ifdef BIN1
 BINS += $(BIN1)
 endif
-
-ifdef BIN2
-BINS += $(BIN2)
-endif
-
 
 ifdef BIN2
 BINS += $(BIN2)
@@ -27,18 +20,57 @@ BINS += $(BIN4)
 endif
 
 #
+# LIBS
+#
+ifdef LIB1
+LIBS += $(LIB1)
+endif
+
+ifdef LIB2
+LIBS += $(LIB2)
+endif
+
+
+ifdef LIB2
+LIBS += $(LIB2)
+endif
+
+
+ifdef LIB3
+LIBS += $(LIB3)
+endif
+
+
+ifdef LIB4
+LIBS += $(LIB4)
+endif
+
+
+
+#
 # build from SRCS 
 #
-SOURCES  :=$(wildcard *.cc)
-OBJECTS  := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(SOURCES))
-BINARYS  := $(patsubst %,$(BUILD_DIR)/%, $(BINS))
-LIBRARYS := $(patsubst %,$(BUILD_DIR)/%, $(LIBS))
+SOURCES  	:=$(wildcard *.cc)
+OBJECTS  	:= $(patsubst %.cc,$(BUILD_DIR)/%.o, $(SOURCES))
+BINARYS  	:= $(patsubst %,$(BUILD_DIR)/%, $(BINS))
+LIBRARYS 	:= $(patsubst %,$(BUILD_DIR)/lib%.so, $(LIBS))
+
+INSTALL_BINARYS := $(patsubst %,$(BIN_DIR)/%, $(BINS))
+INSTALL_LIBRARYS:= $(patsubst %,$(LIB_DIR)/lib%.so, $(LIBS))
 
 
 BIN1_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(BIN1_SOURCES))
 BIN2_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(BIN2_SOURCES))
 BIN3_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(BIN3_SOURCES))
 BIN4_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(BIN4_SOURCES))
+
+LIB1_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(LIB1_SOURCES))
+LIB2_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(LIB2_SOURCES))
+LIB3_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(LIB3_SOURCES))
+LIB4_OBJECTS := $(patsubst %.cc,$(BUILD_DIR)/%.o, $(LIB4_SOURCES))
+
+BIN1_LIBRARYS := $(patsubst %.cc,$(BUILD_DIR)/lib%.so, $(BIN1_LIBS))
+
 
 
 $(BUILD_DIR)/%.o: %.c
@@ -50,16 +82,37 @@ $(BUILD_DIR)/%.o: %.cc
 	$(CCC) -c -o $@ $< $(CCFLAGS)
 
 
+$(BUILD_DIR)/%.o: %.cpp
+	@test -d $(BUILD_DIR) || mkdir -p $(BUILD_DIR)
+	$(CCC) -c -o $@ $< $(CCFLAGS)
+
+
+
+#$(INSTALL_LIBRARYS) : $(LIBRARYS)
+#	cp -p $^ $(LIB_DIR)
+
+#$(INSTALL_BINARYS) : $(BINARYS)
+#	cp -p $^ $(BIN_DIR)
+
+
+#
+# TODO - install HDR rule
+#
+
+
 $(WORKSPACE_DIR)/include/%.mk: %.mk
 	-f $(WORKSPACE_DIR)/include|| mkdir -p $(WORKSPACE_DIR)/include
 	cp -p $^ $(WORKSPACE_DIR)/include
 
 
-.PHONY: clean install debug main all install_hdrs install_bins install_libs
+main: build_libs build_bins
+main: $(OBJECTS) $(BINARYS) $(LIBRARYS) $(HEADERS)
+
 
 all : main
+install : install_libs install_bins 
 
-main: $(OBJECTS) $(BINARYS) $(LIBRARYS) $(HEADERS)
+$(INSTALL_LIBRARYS) : $(LIBRARYS)
 
 
 
--- make/makefile_targets.mk
+++ make/makefile_targets.mk
@@ -1,14 +1,27 @@
+
+
+
+
+ifdef LIB1
+$(BUILD_DIR)/lib$(LIB1).so: $(LIB1_OBJECTS)
+	$(CC) -o$@ $^   $(LDFLAGS) $(CCFLAGS) -lstdc++
+endif
+
+
 #
 # this needs be generated per BIN
+# needs work for lib dependency
+# also - BIN1_LD_LIB_FLAGS -l in the variable
 #
+
 ifdef BIN1
 $(BUILD_DIR)/$(BIN1): $(BIN1_OBJECTS)
-	$(CC) -o$@ $^  $(CCFLAGS) -lstdc++
+	$(CC) -o$@  $^ $(CCFLAGS)  -lstdc++ -v -L./lib -L./build -L/tmp -l$(BIN1_LIBS)
 endif
 
 ifdef BIN2
-$(BUILD_DIR)/$(BIN2): $(BIN2_OBJECTS)
-	$(CC) -o$@ $^  $(CCFLAGS) -lstdc++
+$(BUILD_DIR)/$(BIN2): $(BIN2_OBJECTS) 
+	$(CC) -o$@ $^  $(CCFLAGS) -lstdc++ -v -L./lib -L./build
 endif
 
 ifdef BIN3
@@ -21,6 +34,24 @@ $(BUILD_DIR)/$(BIN4): $(BIN4_OBJECTS)
 	$(CC) -o$@ $^  $(CCFLAGS) -lstdc++
 endif
 
+
+$(INSTALL_LIBRARYS) : $(LIBRARYS)
+	cp -p $^ $(LIB_DIR)
+
+$(INSTALL_BINARYS) : $(BINARYS)
+	cp -p $^ $(BIN_DIR)
+
+
+
+
+
+build_libs : $(LIBRARYS) 
+build_bins : build_libs $(LIBRARYS) $(BINARYS)
+main       : build_libs build_bins
+install_bins : build_bins
+
+
+
 clean:
 	rm -f ./build/*
 	rm -f ./bin/*
@@ -32,22 +63,21 @@ clean:
 #
 
 
-# install: main install_bins install_libs install_hdrs
-# install: main install_bins install_libs install_hdrs
+install_bins : $(INSTALL_BINARYS)
+install_libs : $(INSTALL_LIBRARYS)
+
+#$(INSTALL_BINS) : $(BINARYS)
+#	@test -d  $(BIN_DIR) || mkdir -p $(BIN_DIR)
+#	cp -p $(BINARYS) $(BIN_DIR)
 
 
-install_bins : main
-	@test -d  $(BIN_DIR) || mkdir -p $(BIN_DIR)
-	cp -p $(BINARYS) $(BIN_DIR)
+#install_libs : $(LIBRARYS)
+#	@test -d  $(LIB_DIR) || mkdir -p $(LIB_DIR)
+#	cp -p $(LIBRARYS) $(LIB_DIR)
 
-
-install_libs : main
-	@test -d  $(LIB_DIR) || mkdir -p $(LIB_DIR)
-	cp -p $(LIBRARYS) $(LIB_DIR)
-
-install_hdrs : main
-	@test -d  $(INC_DIR) || mkdir -p $(INC_DIR)
-	cp -p $(HEADERS) $(INC_DIR)
+#install_hdrs : $(HEADERS)
+#	@test -d  $(INC_DIR) || mkdir -p $(INC_DIR)
+#	cp -p $(f) $(INC_DIR)
 
 
 
@@ -57,4 +87,10 @@ debug:
 	@echo "bins:    "  $(BINARYS)
 	@echo "libs:    "  $(LIBRARYS)
 	@echo "headers: "  $(HEADERS)
+	@echo "LIB1_OBJS: "  $(LIB1_OBJECTS)
+	@echo "LIB1_OBJS: "  $(LIB1_SOURCES)
+	@echo "INSTALL_LIBS:" $(INSTALL_LIBRARYS)
+	@echo "BIN1_OBJS:" $(BIN1_OBJECTS) 
+
+
 
