14d63f590f23701c16ecc02dfd7dabbb7dd37513
madaschlesinger <mada.schlesinger@gmail.com>
2017-05-04 13:53:23 -0400

    interim ci

--- c++11/io.cc
+++ c++11/io.cc
@@ -1,3 +1,5 @@
+#include <util/backing_store.h>
+#include <util/memory.h>
 #include <iostream>
 #include <fstream>
 // Shared memory
@@ -23,19 +25,26 @@
 #define FILEPATH "/tmp/mmapped.bin"
 #define NUMINTS  (1000)
 #define FILESIZE (NUMINTS * sizeof(int))
-#include <util/backing_store.h>
+
 
 
 
 using namespace std;
+using util::backing_store::shm_traits;
+using util::backing_store::shm_traits_ro;
+
+using util::backing_store::fs_traits;
+using util::backing_store::fs_traits_ro;
+
+using util::backing_store::store;
+using util::backing_store::traits;
+
+
+
 
 int _file_size() 
 {
   streampos begin,end;
-
-  using util::backing_store::store;
-  using util::backing_store::traits;
-
 
 
   ifstream myfile ( traits::default_fname().c_str(), ios::binary);
@@ -85,192 +94,12 @@ int _binary_read_contents()
 }
 
 
-int _binary_create_backingstore_orig() 
-{
-  int i;
-  int fd;
-  int result;
-  int *map;  /* mmapped array of int's */
-
-  /* Open a file for writing.
-   *  - Creating the file if it doesn't exist.
-   *  - Truncating it to 0 size if it already exists. (not really needed)
-   *
-   * Note: "O_WRONLY" mode is not sufficient when mmaping.
-   */
-  fd = open( "/tmp/backing_store.bin", O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
-  if (fd == -1)
-    {
-      perror("Error opening file for writing");
-      exit(EXIT_FAILURE);
-    }
-
-  /* Stretch the file size to the size of the (mmapped) array of ints
-   */
-  result = lseek(fd, FILESIZE-1, SEEK_SET);
-  if (result == -1)
-    {
-      close(fd);
-      perror("Error calling lseek() to 'stretch' the file");
-      exit(EXIT_FAILURE);
-    }
-
-  /* Something needs to be written at the end of the file to
-   * have the file actually have the new size.
-   * Just writing an empty string at the current file position will do.
-   *
-   * Note:
-   *  - The current position in the file is at the end of the stretched
-   *    file due to the call to lseek().
-   *  - An empty string is actually a single '\0' character, so a zero-byte
-   *    will be written at the last byte of the file.
-   */
-  result = write(fd, "", 1);
-  if (result != 1)
-    {
-      close(fd);
-      perror("Error writing last byte of the file");
-      exit(EXIT_FAILURE);
-    }
-
-  /* Now the file is ready to be mmapped.
-   */
-  map = (int *) mmap(0, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (map == MAP_FAILED)
-    {
-      close(fd);
-      perror("Error mmapping the file");
-      exit(EXIT_FAILURE);
-    }
-
-  /* Now write int's to the file as if it were memory (an array of ints).
-   */
-  for (i = 1; i <=NUMINTS; ++i)
-    {
-      map[i] = 2 * i;
-    }
-
-  /* Don't forget to free the mmapped memory
-   */
-  if (munmap(map, FILESIZE) == -1)
-    {
-      perror("Error un-mmapping the file");
-      /* Decide here whether to close(fd) and exit() or not. Depends... */
-    }
-
-  /* Un-mmaping doesn't close the file, so we still need to do that.
-   */
-  close(fd);
-  return 0;
-}
-
-
-
-int _binary_create_backingstore_non_T() 
-{
-    
-    using util::backing_store::store;
-    using util::backing_store::traits;
-    using util::backing_store::fs_traits_ro;
-    using util::backing_store::fs_traits;
-
-
-    // traits t;
-    store< fs_traits<FILESIZE>> bs; // ( FILEPATH,FILESIZE );
-
-    int *map = (int *) bs.allocate() ; 
-    
-    if (map == nullptr)
-    {
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Now write int's to the file as if it were memory (an array of ints).
-     */
-    for (auto i = 0; i < NUMINTS; ++i)
-    {
-        map[i] = 2 * i ;
-    }
-    
-    // clean up now occurs via dtr of the backing_store object
-    return 0;
-} 
-
-
-
-int _binary_read_backingstore_orig()
-{
-    int i;
-    int fd;
-    int *map;  /* mmapped array of int's */
-    
-    fd = open( "/tmp/backing_store.bin", O_RDONLY);     // FILEPATH, O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error opening file for reading");
-        exit(EXIT_FAILURE);
-    }
-
-    size_t abc = FILESIZE;
-    map = (int *) mmap(0, abc, PROT_READ, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED) {
-        close(fd);
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Read the file int-by-int from the mmap
-     */
-    for (i = 0; i <NUMINTS; ++i) {
-        printf("%d: %d\n", i, map[i]);
-    }
-    
-    if (munmap(map, FILESIZE) == -1) {
-        perror("Error un-mmapping the file");
-    }
-    close(fd);
-    return 0;
-}
-
-
-
-
-int _binary_read_backingstore_non_T()
-{
-    using util::backing_store::store;
-    using util::backing_store::fs_traits_ro;
-    
-
-    store< fs_traits_ro<FILESIZE>> bs; // ( FILEPATH,FILESIZE );
-
-    int *map = (int *) bs.allocate() ; 
-    
-    if (map == nullptr )
-    {
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Read the file int-by-int from the mmap
-     */
-    for (auto i = 1; i <=NUMINTS; ++i) if( i == 1 || i % 50 == 0 )
-                                           printf("%d: %d\n", i, map[i]);
-
-    
-    return 0;
-}
-
 template<typename T>
 int _binary_read_backingstore( const char *tag )
 {
-    using util::backing_store::store;
-    using util::backing_store::shm_traits_ro;
-    
-
     store<T> bs;
 
-    int *map = (int *) bs.allocate() ; 
+    int *map = (int *) bs.open_and_mmap() ; 
     
     if (map == nullptr )
     {
@@ -280,7 +109,7 @@ int _binary_read_backingstore( const char *tag )
 
     /* Read the file int-by-int from the mmap
      */
-    for (auto i = 0; i <NUMINTS; ++i) if( i == 0 || i % 50 == 0 || i == NUMINTS -1 )
+    for (auto i = 0; i <NUMINTS; ++i) if( i == 0 || i % 200 == 0 || i == NUMINTS -1 )
                                           printf("%s %d: %d\n", tag, i, map[i]);
 
     
@@ -291,11 +120,9 @@ template<typename T>
 int _binary_create_backingstore() 
 {
     
-    using util::backing_store::store;
-
     store<T> bs; 
 
-    int *map = (int *) bs.allocate() ; 
+    int *map = (int *) bs.open_and_mmap() ; 
     
     if (map == nullptr)
     {
@@ -314,35 +141,128 @@ int _binary_create_backingstore()
     return 0;
 } 
 
-int main () 
+void   _casserts()
+{    
+    shm_traits smt ; 
+    assert( smt.truncate == true );
+    
+    fs_traits_ro fsro ; 
+    assert( fsro.truncate == false );
+    
+}
+
+void _backing_store()
 {
-    // cerr << "GIT_VERSION: " << GIT_VERSION << endl;
-    //_binary_create_backingstore() ;
-    // _binary_read_backingstore() ;
+    _binary_create_backingstore<fs_traits>() ;
+    _binary_read_backingstore<fs_traits_ro>( "fs" ) ;
+    
+    _binary_create_backingstore<shm_traits>() ;
+    _binary_read_backingstore<shm_traits_ro>( "sm" ) ;
+}
+
+void _allocator()
+{
+    using util::memory::allocator ; 
+
+    allocator<int> a;
+    
+    allocator<int>::value_type abc = 0 ;
+    
+    bool tf = a.has_spare_capacity( 1 ) ;
 
 
-     using util::backing_store::shm_traits;
-     using util::backing_store::shm_traits_ro;
+    // allocators capacity - should be traits::default_capacity.  the stores capacity includes the header
+    assert( a.capacity() == traits::default_capacity() ) ; 
+    
+    assert( a.has_spare_capacity( 1    ) == true ) ; 
+    assert( a.has_spare_capacity( 1023 ) == true ) ; 
+    assert( a.has_spare_capacity( 1024 ) == true ) ; 
 
-     using util::backing_store::fs_traits;
-     using util::backing_store::fs_traits_ro;
-     
-     _binary_create_backingstore<fs_traits<FILESIZE>>() ;
-     _binary_read_backingstore<fs_traits_ro<FILESIZE>>( "fs" ) ;
+    int size_max = a.max_size() ; 
+    assert( size_max == a.capacity() / sizeof( int ) ) ; 
+    
+    int *m = a.allocate( 1024 );
+    int size_c   = a.size() ;
+
+    assert( size_c ==  1024 ) ;     
+    assert( a.has_spare_capacity( size_max -size_c    ) == true  ) ; 
+    assert( a.has_spare_capacity( size_max -size_c +1 ) == false ) ; 
+
+    
+    for (auto i = 0; i < 1024; ++i)
+    {
+        m[i] = 2 * i  ;
+    }
+
+    {
+        
+        using util::backing_store::store;             
+        store<fs_traits_ro> bs;
+        _binary_read_backingstore<fs_traits_ro>( "fsa" ) ;
+    }
+    
+    
+    {
+        allocator<int,fs_traits_ro> a;
+        
+        allocator<int>::value_type abc = 0 ;
+        
+        int *m = a.allocate( 1024 );
+        
+        for (auto i = 0; i < 1024; ++i)
+        {
+            assert( m[i] == 2 * i ) ;
+        }
+    }
+    
+    
+#if 0 
+    try {
+        std::bad_alloc exception;
+        throw ( exception ); 
+    }
+    catch (const std::bad_alloc&) {
+        return -1;
+    }
+    
+#endif
+}
 
 
-     _binary_create_backingstore<shm_traits<FILESIZE>>() ;
-     _binary_read_backingstore<shm_traits_ro>( "sm" ) ;
-
-
-     shm_traits<4> smt ; 
-     assert( smt.truncate == true );
-
-     fs_traits_ro<4> fsro ; 
-     assert( fsro.truncate == false );
-    //_txt() ;
+void _files()
+{
+    _txt() ;
     _file_size();
-    return 0;
+    
+}
+
+int main (int argc, char **argv ) 
+{
+    bool bs       = true ;
+    bool alloc    = true ; 
+    bool casserts = true ; 
+    bool files    = true ; 
+    ++argv;
+    
+    while(argc > 0 && *argv )
+    {
+        string arg =  *argv ;
+        if( arg == "-alloc"   ) bs    = casserts = files = false ; 
+        if( arg == "-bs"      ) alloc = casserts = files = false ; 
+        if( arg == "-files"   ) alloc = casserts = bs    = false ; 
+        if( arg == "-cassert" ) alloc = bs       = files = false ; 
+
+        --argc ;
+        ++argv ;
+    }
+
+    if( bs       ) _backing_store() ;
+    if( files    ) _files()  ; 
+    if( alloc    ) _allocator() ; 
+    if( casserts ) _casserts() ; 
+
+     
+     return 0;
 }
 
 
