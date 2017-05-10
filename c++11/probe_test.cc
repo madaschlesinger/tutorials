73ad0e9a4498f74f24330da07201527c0f3f5999
madaschlesinger <mada.schlesinger@gmail.com>
2017-05-10 13:20:02 -0400

    more tests

--- c++11/probe_test.cc
+++ c++11/probe_test.cc
@@ -29,6 +29,7 @@
 
 
 using namespace std;
+using namespace util;
 
 using util::backing_store::fs_traits;
 using util::probe::stats;
@@ -36,53 +37,113 @@ using util::probe::observation;
 using util::probe::duration;
 using util::probe::occurrence;
 
+void _casserts()
+{
+  {
+    probe::occurrence  p( "occurrence:assert:test:1" ) ;
+
+    const probe::stats * stats = p.statistics() ;
+
+    assert( stats -> min_    == std::numeric_limits<double>::max()  ) ; 
+    assert( stats -> max_    == std::numeric_limits<double>::min()  ) ; 
+    assert( stats -> value_  == 0 ) ; 
+    assert( stats -> t_      == util::probe::type::OCCURRENCE ) ; 
+
+    // stats_ -> value_ = stats_ -> mean_ = stats_ -> std_ = stats_ -> n_ = 0 ;
+  }
+  {
+      probe::duration  d( "duration:assert:test:1" ) ;
+      assert( d.time_point_end()  <  d.time_point_start() );
+      assert( d.time_point_end()  == probe::duration::time_point::min() );
+  }
+
+}
+
+void _occurrence_tests()
+{
+  
+  probe::occurrence  p( "occurrence:test" ) ;
+  
+  for( auto i = 0 ; i < 10 ; ++i )  p.record()  ; 
+
+  assert( p.occurrences() == 10 ) ; 
+
+}
+
+void _duration_tests()
+{
+  
+  probe::duration  d( "duration:tests" ) ; 
+  
+  std::srand( std::time(0) );
+  for( auto i = 0 ; i < 100 ; ++i )
+    {
+      d.start() ; 
+      
+      uint32_t msec = std::rand() % 10 ; 
+      std::this_thread::sleep_for(std::chrono::milliseconds( msec ));
+      
+      d.end() ; 
+      
+      d.record() ; 
+      
+    }        
+  
+  d.report() ; 
+}
+
+void _probe_config_tests()
+{
+
+  // TODO - kill fwd declares....
+  bool read_configuration( const char *filename = nullptr ) ;
+
+  bool result = read_configuration() ;
+
+  assert( result == true ) ; 
+}
+
+
 int main (int argc, char **argv ) 
 {
-    bool bs       = true ;
-    bool alloc    = true ; 
-    bool casserts = true ; 
-    bool files    = true ; 
-    ++argv;
-    
-    while(argc > 0 && *argv )
+  bool bs       = true ;
+  bool alloc    = true ; 
+  
+  bool casserts = true ; 
+  bool probes   = true ;
+  bool cfg      = true ;
+  ++argv;
+  
+  while(argc > 0 && *argv )
     {
-        string arg =  *argv ;
-        if( arg == "-alloc"   ) bs    = casserts = files = false ; 
-        if( arg == "-bs"      ) alloc = casserts = files = false ; 
-        if( arg == "-files"   ) alloc = casserts = bs    = false ; 
-        if( arg == "-cassert" ) alloc = bs       = files = false ; 
+      string arg =  *argv ;
+      if( arg == "-alloc"   ) bs    = casserts = probes = cfg      = false ; 
+      if( arg == "-bs"      ) alloc = casserts = probes = cfg      = false ; 
+      if( arg == "-probes"  ) alloc = casserts = bs     = cfg      = false ; 
+      if( arg == "-cassert" ) alloc = bs       = probes = cfg      = false ; 
+      if( arg == "-config"  ) alloc = bs       = probes = casserts = false ; 
+      
+      --argc ;
+      ++argv ;
+    }
+  
+  _casserts() ;
 
-        --argc ;
-        ++argv ;
+  if( cfg )
+    {
+      _probe_config_tests() ; 
     }
 
-     // observation foo( "foo:bar" ) ; 
-
-     occurrence  goo( "goo:bar" ) ;
-
-     for( auto i = 0 ; i < 10 ; ++i )  goo.record()  ; 
-
-
-     duration  d( "doo:bar" ) ; 
-
-     std::srand( std::time(0) );
-     for( auto i = 0 ; i < 10 ; ++i )
-     {
-         d.start() ; 
-         
-         uint32_t msec = std::rand() % 10 ; 
-         std::this_thread::sleep_for(std::chrono::milliseconds( msec ));
-         
-         d.end() ; 
-         
-         d.record() ; 
-
-     }        
- 
-     goo.report() ; 
-     d.report() ; 
-
-     return 0;
+  if( probes ) 
+    {
+      probe::duration  d( "duration:outer:tests" ) ; 
+      _duration_tests()   ; 
+      _occurrence_tests() ;
+      
+      d.record() ; 
+      d.report() ; 
+    }
+  return 0;
 }
 
 
